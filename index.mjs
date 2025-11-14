43c53eb7ccdaf12ccc8ad9c5c3aa6d658f291f80
David Eraso <davidben.eraso@gmail.com>
2025-11-14 16:03:28 -0500

    added everything from the local folder

--- index.mjs
+++
@@ -1,9 +0,0 @@
-// index.js - ESTE ES EL HANDLER DE TU AWS LAMBDA (index.handler)
-
-import serverless from "serverless-http";
-
-// Importa la funcion 'createServer' desde el código compilado.
-// production.mjs es el nombre de archivo definido en vite.config.server.ts
-import { createServer } from "./server/production.mjs"; 
-
-export const handler = serverless(createServer());
